4f61c900e52866d0b43ca7f00a6dda38fd45b54a
Jcesar1901 <jcesarc19@hotmail.com>
2025-05-13 01:05:32 -0400

    Commit_corrigindo_youtube

--- load_documentos.py
+++ load_documentos.py
@@ -1,6 +1,8 @@
+import re
 import os
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader, YoutubeLoader, CSVLoader,PyPDFLoader, TextLoader
+from youtube_transcript_api import YouTubeTranscriptApi
 from fake_useragent import UserAgent
 from time import sleep
 
@@ -21,11 +23,23 @@ def carregar_site(url):
         st.stop()
     return documento
 
-def carregar_youtube(video_id):
-    loader = YoutubeLoader(video_id, add_video_info = False, language = ['pt'])
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+def carregar_youtube(url):
+    video_id = None
+    match = re.search(r"v=([a-zA-Z0-9_-]+)", url)
+    if match:
+        video_id = match.group(1)
+    else:
+        st.error('Não foi possível extrair o ID do vídeo da URL.')
+        st.stop()
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript = transcript_list.find_transcript(['pt'])
+        entries = transcript.fetch()
+        documento = "\n\n".join(entry.text for entry in entries)
+        return documento
+    except Exception as e:
+        st.error(f"Erro ao carregar a transcrição do vídeo: {str(e)}")
+        st.stop()
 
 def carregar_csv(caminho):
     loader = CSVLoader(caminho)
